3109b011e829ea3f93e14fea3fdacd43c03aa284
audycipta02@gmail.com <audycipta02@gmail.com>
2022-03-23 11:55:06 +0800

    first commit

---
+++ layouts/footer2.php
@@ -0,0 +1,8 @@
+<footer class="main-footer" style="margin-top: 20px;padding-bottom: 20px;">
+	<!-- To the right -->
+	<div class="float-right d-sm-none d-md-block"style="padding: 30px;">
+	<strong>Copyright&copy; Team C 2022</strong>
+	</div>
+	<!-- Default to the left -->
+<br><br>
+</footer>
